7c34ad12b6605fce8922c46219dccddfa26fa2f2
Njambi-M <100689705+Njambi-M@users.noreply.github.com>
2024-04-16 23:30:22 +0300

    feat: add put operation to cats router

--- routes/cats.router.js
+++ routes/cats.router.js
@@ -6,5 +6,6 @@ const catController = require("../controllers/cats.controller")
 router.post('/add/:name', catController.create);
 router.get('/', catController.read);
 router.delete('/cats', catController.delete);
+router.put('/cats/:oldName/:newName', catController.update);
 
 module.exports = router
