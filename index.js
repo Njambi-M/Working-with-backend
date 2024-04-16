12e27e4118ebd3e451226af805a03a9c8cc70480
Njambi-M <100689705+Njambi-M@users.noreply.github.com>
2024-04-16 21:42:02 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+const birds = require('./routes/birds')
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// })
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+app.get('/', (req,res) => {
+res.send('root')
+})
+
+app.get('/about', (req, res) =>{
+    res.send('about')
+})
+
+app.get('/random.text', (req,res) => {
+res.send('random.text')
+})
+
+app.get('/ab?cd', (req,res)=>{
+    res.send('ab?cd')
+})
+
+app.get('/ab+cd', (req,res)=>{
+    res.send("ab+cd")
+})
+app.get(/a/, (req, res) => {
+    res.send('/a/')
+  })
+
+app.get(/.*fly$/, (req, res) => {
+    res.send('/.*fly$/')
+  })
+
