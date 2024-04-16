19d767147a2cf4e695765a6276b02306b747599d
Njambi-M <100689705+Njambi-M@users.noreply.github.com>
2024-04-16 23:52:05 +0300

    feat: add put and delete to the todo router

--- routes/todo.router.js
+++ routes/todo.router.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const todoController = require("./todo.controller");
+const todoController = require("../controllers/todo.controller");
 
 router.post('/', todoController.createTodo);
 router.get('/', todoController.readTodos);
+router.put('/:id', todoController.updateTodo);
+router.delete('/:id', todoController.deleteTodo);
 
 module.exports = router;
